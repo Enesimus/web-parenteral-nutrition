7dbf2545a472463774ead1d540a039068a77e9da
Juan Sepúlveda <jrsepulv@gmail.com>
2020-07-01 01:07:52 -0400

    Creacion de variables, funcion superficie corporal

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # web-parenteral-nutrition
 Desarrollo de una pagina web que calcule una nutricion parenteral pediatrica
 Ha sido complejo entender como funciona la interaccion entre js y el formulario, me siento atascado en el cálculo de la edad, probablemente porque deseo que sea mas automatico de lo que estoy preparado para programar.
+
+Finalmente decidi empezar por definir las variables de entrada, y avanzar en las diferentes funciones que deberá tener la página.
+Me encantaría poder contar con un servidor que me almacene las entradas, y poder contar con un registro de las recetas realizadas. Pensaba en escribir un/varios JSON a modo de "base de datos", que puedan ser leídos posteriormente para generar el informe que puede ser la receta o un comprobante, o incluso crear una "historia de nutricion parenteral".
+
+También deseo incluir un módulo con los cálculos de requerimientos energéticos.
--- js/myscripts.js
+++ js/myscripts.js
@@ -1,3 +1,49 @@
+/* Funciones de Nutricion Parenteral */
+
+let unidad = document.querySelector("#unidad");
+let cama = document.querySelector("#cama");
+let anexo = document.querySelector("#anexo");
+let nombres = document.querySelector("#nombres");
+let apPater = document.querySelector("#appater");
+let apMater= document.querySelector("#apmater");
+let fNac = document.querySelector("#fechanacimiento");
+let run = document.querySelector("#run");
+let fActual = document.querySelector("#fechaactual");
+let peso = document.querySelector("#peso");
+let talla = document.querySelector("#talla");
+let volDes = document.querySelector("#voldes");
+let cargaGluc = document.querySelector("#cg");
+let prots = document.querySelector("#proteinas");
+let lips = document.querySelector("#lipidos");
+let na = document.querySelector("#na");
+let acna = document.querySelector("#acna");
+let k = document.querySelector("#k");
+let kpo = document.querySelector("#kpo");
+let ca = document.querySelector("#ca");
+let mg = document.querySelector("#mg");
+let zn = document.querySelector("#zn");
+let vits = document.querySelector("#vits");
+let ols = document.querySelector("#oe");
+let ciclado = document.querySelector("#ciclado");
+let hrSusp = document.querySelector("#hrsusp");
+
+if (ciclado = "Si") {
+	hrSusp.attrib.hidden = "false";
+} else {}
+
+function calcSC(peso,talla){
+	let supCorp;
+	if (talla !== "") {
+		supCorp = parseFloat((Math.sqrt((peso * talla)**2 / 3600)).toPrecision(2));
+		return supCorp;
+		document.getElementById("scorp").innerHTML = supCorp + " ";
+	} else {
+		supCorp = ((peso * 4)+7)/(90+peso).toPrecision(2);
+		return supCorp;
+		document.getElementById("scorp").innerHTML = supCorp + " ";
+	}
+}
+
 function calcEdad(){
 	var fechaActual = document.getElementById("fechaactual").value;
 	var fechaNacimiento = document.getElementById("fechanacimiento").value;
